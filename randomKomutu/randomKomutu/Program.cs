34133fc4bab6f98ac93544f70e7b20de8e82605e
Doğukan <60947113+Dogukan18@users.noreply.github.com>
2021-10-28 16:50:54 +0300

    Add files via upload

---
+++ randomKomutu/randomKomutu/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace randomKomutu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Random sayi = new Random();
+            int a;
+            a=sayi.Next(0, 101);
+            Console.WriteLine(a);
+            Console.Read(); */
+
+
+            /* Random sayi = new Random();
+             int a, b;
+             a = sayi.Next(0, 101);
+             b = sayi.Next(0, 101);
+             Console.WriteLine(a);
+             Console.WriteLine(b);
+             Console.Read(); */
+
+            /*  Random sayi = new Random();
+              int a;
+              a = sayi.Next(50);
+              Console.WriteLine(a);
+              Console.ReadKey(); */
+
+            /* Random sayi = new Random();
+             int s1;
+             s1 = sayi.Next();
+             Console.WriteLine(s1);
+             Console.ReadKey(); */
+
+
+            
+            
+
+
+        }
+    }
+}
